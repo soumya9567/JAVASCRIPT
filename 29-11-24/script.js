c5c15e242f21b7a417378f19c3bafef20ab60b90
soumya9567 <soumyammu1917@gmail.com>
2024-12-02 14:08:50 +0530

    new loop task commit

--- 29-11-24/script.js
+++ 29-11-24/script.js
@@ -1,36 +1,36 @@
-// var i;
-// console.log("first 10 numbers")
-// for(i=0;i<=10;i++){
-//     console.log(i);
-// }
+var i;
+console.log("first 10 numbers")
+for(i=0;i<=10;i++){
+    console.log(i);
+}
 
-// let a=5;
-// while(a<100){
-//     console.log(a);
-//    a=a+10;
-// }
-
-
-// let b=5;
-// do{
-// console.log(b);
-
-// }while(b>100)
+let a=5;
+while(a<100){
+    console.log(a);
+   a=a+10;
+}
 
 
-// var arr=[1,2,3,4,5];
-// var skip=arr[3]
-// // console.log(arr.length);
+let b=5;
+do{
+console.log(b);
 
-// for(i=0;i<=arr.length;i++){
-//     if(arr[i]==skip){
-//         continue;
-//     }
+}while(b>100)
+
+
+var arr=[1,2,3,4,5];
+var skip=arr[3]
+// console.log(arr.length);
+
+for(i=0;i<=arr.length;i++){
+    if(arr[i]==skip){
+        continue;
+    }
     
-//     console.log(arr[i]);
-//     // i=3;
-//     // break;
-// }
+    console.log(arr[i]);
+    // i=3;
+    // break;
+}
 
 
 var arr1=[1,2,4,6,8,10,11]
@@ -43,17 +43,17 @@ var sliceaft=arr1.slice(0,3);
 console.log(sliceaft);
 
 
-// arr1.push(11,10);
-// console.log(arr1,"push");
+arr1.push(11,10);
+console.log(arr1,"push");
 
-// arr1.pop();
-// console.log(arr1,"pop");
+arr1.pop();
+console.log(arr1,"pop");
 
-// arr1.shift()
-// console.log(arr1,"shift");
+arr1.shift()
+console.log(arr1,"shift");
 
-// arr1.unshift(90,23)
-// console.log(arr1)
+arr1.unshift(90,23)
+console.log(arr1)
 
-// console.log(arr1.slice(0,4),"slice");
-// console.log(arr1.splice(2,4),"splice");
+console.log(arr1.slice(0,4),"slice");
+console.log(arr1.splice(2,4),"splice");
---
+++ 29-11-24/task/script.js
@@ -0,0 +1,137 @@
+// console.log("multiplication table");
+// var num=prompt("enter the number");
+// for(i=1;i<=10;i++){
+//     console.log(i*num);
+// }
+
+// console.log("priint number");
+// var i;
+// for(i=0;i<=20;i++)
+// {
+//     console.log(i);
+// }
+
+
+// console.log("Array value print");
+
+// const arr=["ramu","seethu","janu"];
+// var length=arr.length;
+// console.log(length)
+// var txt="";
+// for(i=0;i<length;i++){
+//     txt=txt+arr[i]+" ";
+    
+// }
+// console.log(txt);
+
+
+// console.log("sum of a number");
+
+// var num3=prompt("enter the number of sum");
+// var sum=0;
+// for( i=1;i<num3;i++){
+
+
+//     sum=sum+i;
+
+// }
+// console.log(sum,"sum of a number");
+
+
+// console.log("number of even number and odd number in array");
+// var arr2=[1,2,3,4,5,6,8,9,10];
+// var odd=0;
+// var even=0;
+// for(i=0;i<arr2.length;i++)
+//     if(arr2[i]%2==0){
+//         even++
+//     }
+//     else{
+//         odd++;
+//     }
+//     console.log("Even numbers: " + even);
+//     console.log("Odd numbers: " + odd);
+
+
+// WHILE LOOP
+
+// console.log("EVEN NUMBER OR ODD NUMBER");
+//  var i=prompt("enter the number");
+//  while(i%2==0){
+//     console.log(i,"even number");
+//     break;
+//  }
+//  if(i%2!=0){
+//  console.log( i,"odd number");
+//  }
+
+
+//  console.log("multiplication");
+//  var multi=prompt("enter the number for multiplication table")
+//  var j=1
+//  while(j<=10){
+//     console.log(j*multi);
+//     j++;
+//  }
+
+//  console.log("sum of a number");
+
+//  var as=prompt("enter the number for sum");
+//  var sum=0;
+//  var k=1;
+//  while(k<as){
+//     sum=sum+k;
+//     k++;
+   
+//  }
+ 
+//  console.log(sum,"sum of a number");
+
+
+//  console.log("print number btw 100-200")
+// var green=0
+// while(green<5){
+//    console.log(green);
+//    green++
+// }
+
+
+// DO WHILE
+
+//  console.log("sum of a number");
+
+//  var as=prompt("enter the number for sum");
+//  var sum=0;
+//  var k=1;
+// do{
+//     sum=sum+k;
+//     k++;
+   
+//  }while(k<as)
+ 
+//  console.log(sum,"sum of a number");
+
+//   console.log("print number btw 100-200")
+// var green=0
+// do{
+//    console.log(green);
+//    green++
+// }while(green<5)
+
+//  console.log("multiplication");
+//  var multi=prompt("enter the number for multiplication table")
+//  var j=1
+//  do{
+//     console.log(j*multi);
+//     j++;
+//  }while(j<=10)
+
+console.log("EVEN NUMBER OR ODD NUMBER");
+ var i=prompt("enter the number");
+do{
+    console.log(i,"even number");
+    break;
+ } while(i%2==0)
+ if(i%2!=0){
+ console.log( i,"odd number");
+ }
