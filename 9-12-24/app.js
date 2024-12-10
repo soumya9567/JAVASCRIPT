d4a5340ddb315b0fa767be4dd0b6312bc99cab2a
soumya9567 <soumyaammu1917@gmail.com>
2024-12-10 12:25:16 +0530

    promise commit

--- 9-12-24/app.js
+++ 9-12-24/app.js
@@ -1,18 +1,84 @@
-const promise = new Promise((resolve,reject)=>{
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("sucess")
+//     }, 1000);
+// })
+// const promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("sucess1")
+//     }, 2000);
+// })
+// const promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         reject("sucess2")
+//     }, 3000);
+// })
+// Promise.all([promise,promise1,promise2]).then((res)=>{
+//     console.log(res)
+// })
+// console.log("promise.allsettled")
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("sucess")
+//     }, 1000);
+// })
+// const promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("sucess1")
+//     }, 2000);
+// })
+// const promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         reject("sucess2")
+//     }, 3000);
+// })
+// Promise.allSettled([promise,promise1,promise2]).then((res)=>{
+//     console.log(res)
+// })
+
+
+// console.log("promise.any")
+
+// const promise = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("sucess")
+//     }, 1000);
+// })
+// const promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("sucess1")
+//     }, 2000);
+// })
+// const promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         reject("sucess2")
+//     }, 3000);
+// })
+// Promise.any([promise,promise1,promise2]).then((res)=>{
+//     console.log(res)
+// })
+
+console.log("promise.race");
+
+const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("sucess")
+        resolve("success")
     }, 1000);
+    
 })
-const promise1 = new Promise((resolve,reject)=>{
+
+const promise1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("sucess1")
+        resolve("success2")
     }, 2000);
 })
-const promise2 = new Promise((resolve,reject)=>{
+
+const promise2 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        reject("sucess2")
+        resolve("success3")
     }, 3000);
 })
-Promise.all([promise,promise1,promise2]).then((res)=>{
-    console.log(res)
-})
+
+Promise.race([promise,promise1,promise2]).then((res)=>
+console.log(res))
+
