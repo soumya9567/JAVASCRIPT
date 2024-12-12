5b05845194f22b256b9d48e07486e6b009997437
soumya9567 <soumyaammu1917@gmail.com>
2024-12-12 12:49:37 +0530

    dummyjson commit

--- 10-12-24/script.js
+++ 10-12-24/script.js
@@ -1,14 +1,42 @@
-async function myfunction(){
-  let  data =await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  let response = await data.json()
-  console.log(response)
-// console.log(data)
-return response;
+// async function myfunction(){
+//   let  data =await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   let response = await data.json()
+//   console.log(response)
+// // console.log(data)
+// return response;
+// }
+
+// // console.log(myfunction());
+//  myfunction().then((value)=>{
+//   console.log(value.title)
+//   let heading = document.getElementById("head1")
+//   heading.innerText= value.title
+//  })
+
+
+async function myfunction() {
+  const response = await fetch('https://dummyjson.com/products');
+  const data = await response.json();
+  console.log(data)
+  
+  const heading = document.getElementById("head1");
+  heading.innerHTML = '';
+
+  data.products.forEach(product => {
+    const para = document.createElement("p");
+    // para.innerText = product.category;
+    para.innerText = product.id;
+    heading.appendChild(para);
+  });
 }
 
-// console.log(myfunction());
- myfunction().then((value)=>{
-  console.log(value.title)
-  let heading = document.getElementById("head1")
-  heading.innerText= value.title
- })
+
+myfunction();
+
+
+
+// var square=arr.map(x=>x*x)
+// let num=arr.filter((value,index)=>value%5!=0)
+
+
+
