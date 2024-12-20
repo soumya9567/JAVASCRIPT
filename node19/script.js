7ff785ca10216d09a0116bacf3596f4a47fbc31a
soumya9567 <soumyaammu1917@gmail.com>
2024-12-20 16:04:41 +0530

    node commit

--- node19/script.js
+++ node19/script.js
@@ -1,6 +1,7 @@
 async function handleSubmit(event){
     event.preventDefault();
-    const todo  = document.getElementById("todo");
+    const todo  = document.getElementById("todo").value;
+    try{
     const response = await fetch("http://localhost:3000",{
         method :"POST",
         headers:{ "content-type":'apllication/JSON'},
@@ -8,4 +9,22 @@ async function handleSubmit(event){
         
         
     })
+    if(!response.ok){
+        throw new error("netwok issue")
+    }
+}
+catch(error){
+    console.log(error,"error")
+
+   
+}
+}
+
+
+async function fetchData(){
+    const response = await fetch("http://localhost:3000/getadd",{
+        method :"GET"
+    })
+
+
 }
--- node19/server.js
+++ node19/server.js
@@ -1,14 +1,30 @@
 const http = require("http");
 const fs = require('fs')
-const path=require('path')
+const path=require('path');
+
+
+
+const newpath=path.join(__dirname,"todo.json")
+async function readjsonfile() {
+    if (!fs.existsSync(newpath)) {
+        fs.writeFileSync(newpath, JSON.stringify([])); // Create an empty array if file does not exist
+    }
+    const fileContent = fs.readFileSync(newpath, 'utf-8'); // Read the file content
+    const data = JSON.parse(fileContent); // Parse the content into a JavaScript array
+    return data;
+}
+
+
 
 const filePath=path.join(__dirname,'index.html')
+const scriptPath=path.join(__dirname,'script.js')
+
  const server=http.createServer((request,response)=>{
     console.log(request.url,"req url")
     console.log(request.method,"request method")
  
 
-    if(request.url==="/" && request.methhod=== "GET"){
+    if(request.url==="/" && request.method=== "GET"){
     fs.readFile(filePath,'utf-8',(error,data)=>{
         if(error){
             response.end("file not found")
@@ -19,20 +35,37 @@ const filePath=path.join(__dirname,'index.html')
     // console.log(request.url)
     if(request.url==="/script.js"&& request.method==="GET"){
 
-        fs.readFile(__dirname ,"utf-8","script.js",(error,data)=>{
+        fs.readFile(scriptPath,"utf-8",(error,data)=>{
             if(error){
                 response.end("file not found")
             }
             response.end(data)
         })
-        }
+} else
+if(request.url==="/getadd" && request.method==="POST"){
+    console.log("enter the path")
+    let body = " ";
+    request.on("data", (chunk)=>{
+        body+=chunk;
+    })
+    console.log(body,"body data");
+
+
+    request.on("end", async () => {
         
-});
+            const todo = await readjsonfile();
+            console.log("Existing todo data:", todo);
+
+            const parsedBody = JSON.parse(body);
+            console.log("Received data:", parsedBody);
 
 
+ })}
+        
+;
 
-     
 
 server.listen(3000,()=>{
     console.log("listen port 3000")
-});
+})
+ })
